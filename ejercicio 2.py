4ec90e896d598454ef1ec9743705089a5b97e03b
pabloramon4esoA <144780812+pabloramon4esoA@users.noreply.github.com>
2023-09-12 10:45:56 +0200

    Add files via upload

---
+++ ejercicio 2.py
@@ -0,0 +1,13 @@
+#2. Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla en el siguiente orden y formato: número entero, texto y número decimal.
+variable1=int(input ("el valor introducido es un: "))
+variable2=input ("el valor introducido es la letra: ")
+variable3=float(input("el valor introducido es el número decimal: "))
+print("El valor introducido es un",variable1)
+print("El valor introducido es la letra", variable2)
+print("el valor introducido es el número decimal",variable3)
+
+
+
+             
+
+            
